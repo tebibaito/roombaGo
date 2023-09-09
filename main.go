525e6176cc5f1d9c112255bc59a0a56e759e4e60
tebibaito <tebibaito@gmail.com>
2023-09-09 16:06:03 +0900

    add poweroff handler

--- main.go
+++ main.go
@@ -33,6 +33,7 @@ func main() {
 	http.HandleFunc("/clean", cleanHandler(ser))
 	http.HandleFunc("/dock", dockHandler(ser))
 	http.HandleFunc("/battery", getBatteryHandler(ser))
+	http.HandleFunc("/poweroff", powerOffHandler(ser))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -52,14 +53,24 @@ func wakeUp(ser *serial.Port) {
 		pin.High()
 		time.Sleep(2 * time.Second)
 	}
-	// スタートコマンド
-	start := []byte{128}
-	sendCommand(ser, start)
-
 	// 充電中はスタートコマンドだけでは反応しなのでcleanコマンドを実行
 	if data[34] > 0 {
 		clean(ser)
+		time.Sleep(300 * time.Millisecond)
+	} else {
+		// スタートコマンド
+		start := []byte{128}
+		sendCommand(ser, start)
 	}
+}
+
+func getIsOn(ser *serial.Port) bool {
+	mode := SensorPacket{packetId: 35, dataBytes: 1}
+	_, err := readSensor(ser, []SensorPacket{mode})
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func clean(ser *serial.Port) {
@@ -74,26 +85,50 @@ func dock(ser *serial.Port) {
 	sendCommand(ser, command)
 }
 
+func powerOff(ser *serial.Port) {
+	command := []byte{133}
+	sendCommand(ser, command)
+}
+
+func powerOffHandler(ser *serial.Port) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		powerOff(ser)
+	}
+}
+
 func cleanHandler(ser *serial.Port) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if isOn := getIsOn(ser); isOn == true {
+			powerOff(ser)
+			time.Sleep(100 * time.Millisecond)
+		}
 		wakeUp(ser)
+		time.Sleep(100 * time.Millisecond)
 		clean(ser)
+		fmt.Fprint(w, "start cleaning!")
 	}
 }
 
 func dockHandler(ser *serial.Port) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if isOn := getIsOn(ser); isOn == true {
+			powerOff(ser)
+			time.Sleep(1000 * time.Millisecond)
+		}
 		wakeUp(ser)
+		time.Sleep(100 * time.Millisecond)
 		dock(ser)
+		fmt.Fprint(w, "back to homebase!")
 	}
 }
 
 func sendCommand(ser *serial.Port, command []byte) {
-	time.Sleep(50 * time.Millisecond)
+	// time.Sleep(50 * time.Millisecond)
 	_, err := ser.Write(command)
 	if err != nil {
 		fmt.Println(err)
 	}
+	log.Printf("send %v", command)
 }
 
 type SensorPacket struct {
@@ -118,12 +153,7 @@ func readSensor(ser *serial.Port, packets []SensorPacket) (map[int]int, error) {
 	var receiveCnt int = (totalDataBytes / 8) + 1
 
 	// データ送信
-	_, err := ser.Write(serSequence)
-	if err != nil {
-		// fmt.Println("serial data receive failed")
-		return nil, errors.New("serial data send failed")
-	}
-
+	sendCommand(ser, serSequence)
 	time.Sleep(100 * time.Millisecond)
 
 	var received []byte
@@ -158,8 +188,7 @@ func readSensor(ser *serial.Port, packets []SensorPacket) (map[int]int, error) {
 
 		time.Sleep(5 * time.Millisecond)
 	}
-	fmt.Printf("%v\n", received)
-	// fmt.Printf("%d\n", len(received))
+	log.Printf("%v\n", received)
 	// エンコード
 	result := map[int]int{}
 	for i := 2; i < len(received)-1; {
@@ -174,6 +203,7 @@ func readSensor(ser *serial.Port, packets []SensorPacket) (map[int]int, error) {
 		result[int(packetId)] = val
 		i += databytes
 	}
+	log.Printf("%v", result)
 	return result, nil
 }
 
@@ -189,7 +219,7 @@ func getBatteryData(ser *serial.Port) BatteryData {
 	packets := []SensorPacket{chargePacket, capacityPacket}
 	result, err := readSensor(ser, packets)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 		return batteryData
 	}
 	batteryData.Capacity = result[int(capacityPacket.packetId)]
@@ -199,7 +229,7 @@ func getBatteryData(ser *serial.Port) BatteryData {
 
 func getBatteryHandler(ser *serial.Port) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		wakeUp(ser)
+		// wakeUp(ser)
 		batteryData := getBatteryData(ser)
 		var buf bytes.Buffer
 		enc := json.NewEncoder(&buf)
